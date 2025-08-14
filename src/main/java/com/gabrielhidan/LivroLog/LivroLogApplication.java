39ba04c8cd0d0a371ca63d68afe40ad7b0b6ae15
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-13 21:38:51 -0300

    first commit

---
+++ src/main/java/com/gabrielhidan/LivroLog/LivroLogApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielhidan.LivroLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivroLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivroLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielhidan/LivroLog/LivroLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielhidan.LivroLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivroLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
