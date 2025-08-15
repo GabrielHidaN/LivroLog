1f4ea6c7e05629406d80d739ac48bd3bf353abca
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-15 00:58:08 -0300

    feat: cria o BooksService e adiciona o primeiro service de criacao de novo um livro , usando o JPA para persistencia salvando o livro no banco de dados.

--- src/main/java/com/gabrielhidan/LivroLog/books/service/BooksService.java
+++ src/main/java/com/gabrielhidan/LivroLog/books/service/BooksService.java
@@ -1,4 +1,32 @@
 package com.gabrielhidan.LivroLog.books.service;
+import com.gabrielhidan.LivroLog.books.entities.Books;
+import com.gabrielhidan.LivroLog.books.repository.BooksRepository;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class BooksService {
+
+    @Autowired
+    private final BooksRepository booksRepository;
+
+    // getAllBooks
+
+    public List<Books> getAllBooks(){
+        return booksRepository.findAll();
+    }
+
+    // create new Book
+
+    public Books createNewBook(Books books){
+        return booksRepository.save(books);
+    }
+
 }
