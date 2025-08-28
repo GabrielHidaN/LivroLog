50d8e35185a9f44f132d1ac8d3601ef7a68b0af0
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-27 22:52:24 -0300

    refactor: corrige o nome da Classe CategoryRepository

--- src/main/java/com/gabrielhidan/LivroLog/category/repository/CategoryRepository.java
+++ src/main/java/com/gabrielhidan/LivroLog/category/repository/CategoryRepository.java
@@ -3,5 +3,5 @@ package com.gabrielhidan.LivroLog.category.repository;
 import com.gabrielhidan.LivroLog.category.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategotyRepository extends JpaRepository<Category , Long> {
+public interface CategoryRepository extends JpaRepository<Category , Long> {
 }
