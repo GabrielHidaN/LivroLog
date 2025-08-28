d8bdef063b72eb304ae099ac03d62e81cd17a28c
GabrielHidaN <victorbielgv7@gmail.com>
2025-08-27 22:45:10 -0300

    feat: cria as regras de negocio do category , e cria todos os end points

--- src/main/java/com/gabrielhidan/LivroLog/category/controller/CategoryController.java
+++ src/main/java/com/gabrielhidan/LivroLog/category/controller/CategoryController.java
@@ -1,4 +1,40 @@
 package com.gabrielhidan.LivroLog.category.controller;
+import com.gabrielhidan.LivroLog.category.entities.Category;
+import com.gabrielhidan.LivroLog.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("/create")
+    public Category createCategory(Long id , @RequestBody Category category){
+        return categoryService.createCategory(category);
+    }
+
+    @PostMapping("/update/{id}")
+    public Category updateCategory (@PathVariable  Long id , @RequestBody Category category){
+        return categoryService.updateCategory(id , category);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCategory (@PathVariable Long id){
+        categoryService.deleteCategory(id);
+    }
+
+    @GetMapping()
+    public List<Category> getAllCategory(){
+        return categoryService.getAllCategory();
+    }
+
+    @GetMapping("/{id}")
+    public Category getCategoryById (@PathVariable Long id){
+        return categoryService.getCategoryById(id);
+    }
 }
--- src/main/java/com/gabrielhidan/LivroLog/category/service/CategoryService.java
+++ src/main/java/com/gabrielhidan/LivroLog/category/service/CategoryService.java
@@ -1,4 +1,48 @@
 package com.gabrielhidan.LivroLog.category.service;
+import com.gabrielhidan.LivroLog.category.entities.Category;
+import com.gabrielhidan.LivroLog.category.repository.CategoryRepository;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category){
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategory(){
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategoryById(Long id){
+        Optional<Category> getById = categoryRepository.findById(id);
+        if (getById.isPresent()){
+            return getById.orElse(null);
+        }
+        return null;
+    }
+
+    public void deleteCategory(Long id){
+        Optional<Category> getById = categoryRepository.findById(id);
+        if (getById.isPresent()){
+            categoryRepository.deleteById(id);
+        }
+    }
+
+    public Category updateCategory (Long id , Category category){
+        Optional<Category> getById = categoryRepository.findById(id);
+        if (getById.isPresent()){
+            category.setId(id);
+            return categoryRepository.save(category);
+        }
+        return null;
+    }
 }
